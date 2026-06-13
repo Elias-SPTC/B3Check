0249ec36d42614be5c1b27632149f868bb48215f
elias <elias2007eccshy@gmail.com>
2026-06-13 18:19:26 -0300

    Gerar arquivo jar para Windows/Linux.

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -11,7 +11,11 @@ kotlin {
         val desktopMain by getting {
             dependencies {
                 implementation(project(":shared"))
-                implementation(compose.desktop.currentOs)
+                
+                // Inclui binários para Linux e Windows no mesmo JAR
+                implementation(compose.desktop.linux_x64)
+                implementation(compose.desktop.windows_x64)
+                
                 implementation("com.google.code.gson:gson:2.10.1")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.9.0")
             }
@@ -23,7 +27,7 @@ compose.desktop {
     application {
         mainClass = "MainKt"
         nativeDistributions {
-            targetFormats(org.jetbrains.compose.desktop.application.dsl.TargetFormat.Dmg, org.jetbrains.compose.desktop.application.dsl.TargetFormat.Msi, org.jetbrains.compose.desktop.application.dsl.TargetFormat.Deb)
+            targetFormats(org.jetbrains.compose.desktop.application.dsl.TargetFormat.Msi, org.jetbrains.compose.desktop.application.dsl.TargetFormat.Deb)
             packageName = "B3Check"
             packageVersion = "1.0.0"
         }
